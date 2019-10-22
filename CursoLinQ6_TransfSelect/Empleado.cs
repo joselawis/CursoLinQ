ebdca7314e0439a29be3bdb5c8466c24bd605eac
lawis <joselawis@gmail.com>
2019-10-22 20:58:58 +0200

    Transform Select

---
+++ CursoLinQ6_TransfSelect/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ6_TransfSelect
+{
+    public class Empleado
+    {
+        public string nombreCompleto { get; set; }
+        public string ciudad { get; set; }
+        public int sueldo { get; set; }
+    }
+}
---
+++ CursoLinQ6_TransfSelect/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ6_TransfSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empleado> empleados = new List<Empleado>
+            {
+                new Empleado {nombreCompleto="Tony Stark", sueldo=10200},
+                new Empleado {nombreCompleto="Thor Odinsson", sueldo=1200},
+                new Empleado {nombreCompleto="Bruce Banner", sueldo=2200},
+                new Empleado {nombreCompleto="Peter Parker", sueldo=600}
+            };
+
+            // nombrepersona gana sueldo
+            IEnumerable<String> consulta = from empleado in empleados
+                                             select empleado.nombreCompleto + " gana " + empleado.sueldo;
+
+            foreach(String i in consulta)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
