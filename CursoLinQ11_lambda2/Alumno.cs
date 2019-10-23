e129831c7cf921c4300f44c032d82d53b15074b6
lawis <joselawis@gmail.com>
2019-10-23 21:09:16 +0200

    otro lambda con select

---
+++ CursoLinQ11_lambda2/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ11_lambda2
+{
+    class Alumno
+    {
+        public int idAlumno { get; set; }
+        public string cursoFavorito { get; set; }
+    }
+}
---
+++ CursoLinQ11_lambda2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ11_lambda2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> listaAlumnos = new List<Alumno>
+            {
+                new Alumno{idAlumno=1, cursoFavorito="Matematicas"},
+                new Alumno{idAlumno=2, cursoFavorito="Tecnologia"},
+                new Alumno{idAlumno=3, cursoFavorito="Historia"},
+                new Alumno{idAlumno=4, cursoFavorito="Filosofia"},
+                new Alumno{idAlumno=5, cursoFavorito="Fisica"}
+            };
+
+            // Su curso favorito es Matematicas
+            var consulta = listaAlumnos.Select(a => { return "Su curso favorito es " + a.cursoFavorito; });
+
+            foreach(var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
