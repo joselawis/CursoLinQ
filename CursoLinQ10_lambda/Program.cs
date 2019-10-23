968e3ba566167d3987347daa8844023f264e8ece
lawis <joselawis@gmail.com>
2019-10-23 21:00:08 +0200

    Primer ejercicio haciendo un select con lambdas

---
+++ CursoLinQ10_lambda/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ10_lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customers> listaCustomers = new List<Customers>
+            {
+                new Customers {nombre="Ricardo", apellido="Pelaes", ciudad="Madriz"},
+                new Customers {nombre="Lucia", apellido="Trompeta del sol", ciudad="Valencia"},
+                new Customers {nombre="Fran", apellido="Ibañez", ciudad="Bilbao"}
+            };
+
+            var consulta = listaCustomers.Select(p => p.nombre);
+
+            foreach(var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
